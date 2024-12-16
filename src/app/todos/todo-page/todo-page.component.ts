f9f61e00c78cff83adbe8ed34eb45db2c8c9c723
C.Manuel Hernández <cmanuelhm87@gmail.com>
2024-12-16 12:23:46 +0100

    Toggle All - Marcar o Desmarcar Todos

--- src/app/todos/todo-page/todo-page.component.ts
+++ src/app/todos/todo-page/todo-page.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../app.reducer';
+import * as actions from '../todo.actions';
 
 @Component({
   selector: 'app-todo-page',
   templateUrl: './todo-page.component.html',
-  styleUrl: './todo-page.component.css'
+  styleUrl: './todo-page.component.css',
 })
 export class TodoPageComponent {
+  constructor(private store: Store<AppState>) {}
+  completado: boolean = false;
 
+  toggleAll() {
+    this.completado = !this.completado;
+    this.store.dispatch(
+      actions.toggleAll({
+        completado: this.completado,
+      })
+    );
+  }
 }
--- src/app/todos/todo.reducer.ts
+++ src/app/todos/todo.reducer.ts
@@ -1,5 +1,5 @@
 import { createReducer, on } from '@ngrx/store';
-import { borrar, crear, editar, toggle } from './todo.actions';
+import { borrar, crear, editar, toggle, toggleAll } from './todo.actions';
 import { Todo } from './models/todo.model';
 
 export const estadoInicial: Todo[] = [
@@ -13,6 +13,14 @@ export const todoReducer = createReducer(
   estadoInicial,
   on(crear, (state, { texto }) => [...state, new Todo(texto)]),
   on(borrar, (state, { id }) => state.filter((todo) => todo.id !== id)),
+  on(toggleAll, (state, { completado }) =>
+    state.map((todo) => {
+      return {
+        ...todo,
+        completado: completado,
+      };
+    })
+  ),
   on(toggle, (state, { id }) => {
     return state.map((todo) => {
       if (todo.id === id) {
