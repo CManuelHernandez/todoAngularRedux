8fcb1f55e1e1795fae982a4d767d0474dd456431
C.Manuel Hernández <cmanuelhm87@gmail.com>
2024-12-16 10:23:53 +0100

    nstalar NgRx, modelo, acciones y reducer de Todos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoModule } from './todos/todo.module';
 import { FooterComponent } from './footer/footer.component';
+import { StoreModule } from '@ngrx/store';
+import { todoReducer } from './todos/todo.reducer';
 
 @NgModule({
   declarations: [AppComponent, FooterComponent],
-  imports: [BrowserModule, AppRoutingModule, TodoModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    TodoModule,
+    StoreModule.forRoot({ count: todoReducer }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
