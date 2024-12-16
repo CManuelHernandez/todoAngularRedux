59d2114319d45cc95e846ee07b9308c0f3482115
C.Manuel Hernández <cmanuelhm87@gmail.com>
2024-12-16 13:17:42 +0100

    Cambiar el filtro - únicamente el string

--- src/app/todos/todo-footer/todo-footer.component.ts
+++ src/app/todos/todo-footer/todo-footer.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../app.reducer';
+import { setFiltro } from '../../filtro/filtro.actions';
 
 @Component({
   selector: 'app-todo-footer',
   templateUrl: './todo-footer.component.html',
-  styleUrl: './todo-footer.component.css'
+  styleUrl: './todo-footer.component.css',
 })
-export class TodoFooterComponent {
+export class TodoFooterComponent implements OnInit {
+  filtroActual: string = 'todos';
+  filtros: string[] = ['todos', 'completados', 'pendientes'];
 
+  constructor(private store: Store<AppState>) {}
+  ngOnInit(): void {
+    this.store.select('filtro').subscribe((filtro) => {
+      this.filtroActual = filtro;
+    });
+  }
+
+  cambiarFiltro(filtro: string) {
+    this.store.dispatch(setFiltro({ filtro: filtro }));
+  }
 }
